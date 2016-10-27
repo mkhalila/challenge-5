c326985555b608e90abb529b0866d8566d23c491
Khalil Ahmed <mkhalila@outlook.com>
2016-10-27 22:51:35 +0100

    Completes initialisation of board with every square having a set

--- Sudoku.h
+++ Sudoku.h
@@ -9,6 +9,10 @@ using std::vector;
 #include <set>
 using std::set;
 
+#include <iostream>
+using std::cout;
+using std::endl;
+
 // TODO: Your Sudoku class goes here:
 class Sudoku {
 
@@ -18,7 +22,23 @@ private:
 
 public:
 	Sudoku(const int & sizeIn) 
-	: size (sizeIn) {}
+	: size (sizeIn) {
+		//Initialise 2d board of given size
+		board.resize(size);
+		for (size_t i = 0; i < size; ++i) {
+        	board[i] = vector< set<int> >(size);
+    	}
+
+    	//Creates set containing (1.. size) ints
+    	set<int> initSet;
+
+    	//Sets every square to have a set (1.. size)
+    	for (size_t row = 0; row < size; ++row) {
+        	for (size_t col = 0; col < size; ++col) {
+            	board[row][col] = initSet;
+        	}
+    	}	
+	}
 };
 
 
