2a4083cc41d99f4f27a877c43b78aaf5eda1ef59
Khalil Ahmed <mkhalila@outlook.com>
2016-10-27 22:00:55 +0100

    Creates initial constructor and initial memb vars for Sudoku class

--- Sudoku.h
+++ Sudoku.h
@@ -3,9 +3,23 @@
 
 #include "Searchable.h"
 
+#include <vector>
+using std::vector;
+
+#include <set>
+using std::set;
+
 // TODO: Your Sudoku class goes here:
+class Sudoku {
 
+private:
+	int size;
+	vector< vector< set<int> > > board;
 
+public:
+	Sudoku(const int & sizeIn) 
+	: size (sizeIn) {}
+};
 
 
 #endif
