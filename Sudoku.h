02276bb6072a1328b8ee9889a09a553616d9abea
Khalil Ahmed <mkhalila@outlook.com>
2016-11-09 10:44:52 +0000

    Reimplements remove 2 dup. sets in same box

--- Sudoku.h
+++ Sudoku.h
@@ -156,7 +156,7 @@ public:
 						}
 
 						//Look for another set of size 2 in sub-grid
-						/*int rowStart = sqrt(size) * floor(x/sqrt(size));
+						int rowStart = sqrt(size) * floor(x/sqrt(size));
     					int colStart = sqrt(size) * floor(y/sqrt(size)); 
     					int foundX = -1;
     					int foundY = -1;
@@ -183,7 +183,7 @@ public:
 									board[i][j].erase(second);
 								}
 							}
-						}*/
+						}
 
 					}
 
